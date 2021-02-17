74af49ad6f94da033617d72feab4b51f203362d8
Aliceravier <aliceravier@yahoo.co.uk>
2021-02-17 13:50:42 +0000

    Add Urho3D sample project and splash project
    The contents of the sample project directory can be copied to create
    a new project and gives the required structure for a Urho3D project.
    Use cmake to build the project as follows:
    create a build folder
    execute ./cmake_generic.sh PATH_TO_YOUR_BUILD_FOLDER from inside the project folder
    execute cmake --buid . from inside the build folder
    
    The splash project is an example project which uses this technique to show a simple message on the screen

---
+++ Urho3D/splash/splash.cpp
@@ -0,0 +1,128 @@
+//
+// Copyright (c) 2008-2020 the Urho3D project.
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE.
+//
+
+#include <string>
+#include <memory>
+#include <fstream>
+#include <sstream>
+
+#include <Urho3D/Urho3D.h>
+#include <Urho3D/Core/CoreEvents.h>
+#include <Urho3D/Graphics/RenderPath.h>
+#include <Urho3D/Engine/Application.h>
+#include <Urho3D/Engine/Engine.h>
+#include <Urho3D/Input/Input.h>
+#include <Urho3D/Input/InputEvents.h>
+#include <Urho3D/Resource/ResourceCache.h>
+#include <Urho3D/Resource/XMLFile.h>
+#include <Urho3D/IO/Log.h>
+#include <Urho3D/UI/UI.h>
+#include <Urho3D/UI/Text.h>
+#include <Urho3D/UI/Font.h>
+#include <Urho3D/UI/Button.h>
+#include <Urho3D/UI/UIEvents.h>
+#include <Urho3D/UI/Window.h>
+#include <Urho3D/Scene/Scene.h>
+#include <Urho3D/Scene/SceneEvents.h>
+#include <Urho3D/Graphics/Graphics.h>
+#include <Urho3D/Graphics/Camera.h>
+#include <Urho3D/Graphics/Geometry.h>
+#include <Urho3D/Graphics/Renderer.h>
+#include <Urho3D/Graphics/DebugRenderer.h>
+#include <Urho3D/Graphics/Octree.h>
+#include <Urho3D/Graphics/Light.h>
+#include <Urho3D/Graphics/Model.h>
+#include <Urho3D/Graphics/StaticModel.h>
+#include <Urho3D/Graphics/Material.h>
+#include <Urho3D/Graphics/Skybox.h>
+#include <Urho3D/Graphics/Zone.h>
+#include <Urho3D/Audio/Sound.h>
+#include <Urho3D/Audio/SoundSource3D.h>
+#include <Urho3D/Audio/SoundListener.h>
+#include <Urho3D/Audio/Audio.h>
+#include <Urho3D/Graphics/ParticleEmitter.h>
+#include <Urho3D/Graphics/ParticleEffect.h>
+#include <Urho3D/Graphics/Terrain.h>
+
+#include "splash.h"
+
+#include <Urho3D/DebugNew.h>
+
+// Expands to this example's entry-point
+URHO3D_DEFINE_APPLICATION_MAIN(HelloWorld)
+
+HelloWorld::HelloWorld(Context* context) :
+    Sample(context)
+{
+}
+
+void HelloWorld::Start()
+{
+    // Execute base class startup
+    Sample::Start();
+
+    // Create "Hello World" Text
+    CreateText();
+
+    // Finally subscribe to the update event. 
+    // Note that by subscribing events at this point we have already missed some events
+    // like the ScreenMode event sent by the Graphics subsystem when opening the application window. 
+    // To catch those as well we
+    // could subscribe in the constructor instead.
+    SubscribeToEvents();
+
+    // Set the mouse mode to use in the sample
+    Sample::InitMouseMode(MM_FREE);
+}
+
+void HelloWorld::CreateText()
+{
+    auto* cache = GetSubsystem<ResourceCache>();
+
+    // Construct new Text object
+    SharedPtr<Text> helloText(new Text(context_));
+
+    // Set String to display
+    helloText->SetText("Welcome to Sound Pirates!");
+
+    // Set font and text color
+    helloText->SetFont(cache->GetResource<Font>("Fonts/Anonymous Pro.ttf"), 30);
+    helloText->SetColor(Color(0.0f, 1.0f, 1.0f));
+
+    // Align Text center-screen
+    helloText->SetHorizontalAlignment(HA_CENTER);
+    helloText->SetVerticalAlignment(VA_CENTER);
+
+    // Add Text instance to the UI root element
+    GetSubsystem<UI>()->GetRoot()->AddChild(helloText);
+}
+
+void HelloWorld::SubscribeToEvents()
+{
+    // Subscribe HandleUpdate() function for processing update events
+    SubscribeToEvent(E_UPDATE, URHO3D_HANDLER(HelloWorld, HandleUpdate));
+}
+
+void HelloWorld::HandleUpdate(StringHash eventType, VariantMap& eventData)
+{
+    // Do nothing for now, could be extended to eg. animate the display
+}
