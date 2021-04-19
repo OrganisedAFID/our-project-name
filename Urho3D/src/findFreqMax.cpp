46afbff0b67b313c421f8c52294ab04c75b8a368
Fai <faij2009@yahoo.co.uk>
2021-04-19 21:05:15 +0100

    modified freqMax function to include for loop

--- Urho3D/src/findFreqMax.cpp
+++ Urho3D/src/findFreqMax.cpp
@@ -7,9 +7,27 @@
  
 #include <vector>
 
-int findFreqMax(int freqMax, int i, std::vector<double> &window){
+int freqMaxIndex = 51;
+int amplitudeThreshold = 45000; 
 
- freqMax = i* 44100.0 / window.size();
+
+
+
+int findFreqMax(int freqMax,std::vector<double> output, std::vector<double> &window){
+
+ for (int i = 51; i < 100; i++) //i values bewteen 51 and 99 value means that loop is starting to look for the highest frequency values between 255 and 495 hertz, respectively. 
+    {
+
+          
+    
+        if (output[i] >= output[freqMaxIndex] && output[i] > amplitudeThreshold)
+        {
+            freqMaxIndex = i;  
+            freqMax = i* 44100.0 / window.size();      //dividing sampling frquency with buffer length to get frequency resoltion then muliplting with the index values
+                                                      //(of the vector "output" from 51 and 99 to find max frequency between 255 and495 hertz        
+        }
+    } 
+
 
 return freqMax;
 }
